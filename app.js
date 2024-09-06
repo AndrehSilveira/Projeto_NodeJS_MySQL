1660a99ae38207a7ee79d3afbcfa28bb9933ab36
André Silveira <andresilveira.email@gmail.com>
2024-09-05 22:37:15 -0300

    Initial commit

---
+++ app.js
@@ -0,0 +1,41 @@
+// Importar módulo express
+const express = require('express');
+
+// IMPORTAR MÓDULO EXPRESS-HANDLEBARS
+const { engine } = require('express-handlebars');
+
+// IMPORTAR MÓDULO MYSQL
+const mysql = require('mysql2');
+
+// App
+const app = express();
+
+// Adicionar Bootstrap
+app.use('/bootstrap', express.static('./node_modules/bootstrap/dist'));
+
+// CONFIGURAÇÃO DO EXPRESS-HANDLEBAR
+app.engine('handlebars', engine());
+app.set('view engine', 'handlebars');
+app.set('views', './views');
+
+// Configuração de conexão
+const conexao = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'admin123',
+    database: 'projetoEstudoNode'
+});
+
+// TESTE DE CONEXÃO
+conexao.connect(function(erro){
+    if(erro) throw erro;
+    console.log('Conexão efetuada com sucesso.');
+})
+
+// Rota Principal
+app.get('/', (req, res) => {
+    res.render('formulario');
+});
+
+// Servidor
+app.listen(8080);
