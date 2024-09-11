506a78bd8886add009538ccc6bd46c0e82ae8ea6
André Silveira <andresilveira.email@gmail.com>
2024-09-10 21:27:13 -0300

    Mostrando produtos no front
    Foi feita a inclusão das imagens, a criação do banco de dados, a consulta no banco, o armazenamento do retorno do banco em JSON e o laço de repetição no front para exibir as informações em tela.

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 // Importar módulo express
 const express = require('express');
 
+// Importar módulo fileupload
+const fileupload = require('express-fileupload')
+
 // IMPORTAR MÓDULO EXPRESS-HANDLEBARS
 const { engine } = require('express-handlebars');
 
@@ -10,11 +13,17 @@ const mysql = require('mysql2');
 // App
 const app = express();
 
+// Habilitando o upload de arquivos
+app.use(fileupload());
+
 // Adicionar Bootstrap
 app.use('/bootstrap', express.static('./node_modules/bootstrap/dist'));
 
 // Adicionar CSS referência
 app.use('/css', express.static('./css'));
+
+// Referenciar a pasta de imagens
+app.use('/imagens', express.static('./imagens'));
 
 // CONFIGURAÇÃO DO EXPRESS-HANDLEBAR
 app.engine('handlebars', engine());
@@ -41,13 +50,39 @@ conexao.connect(function(erro){
 
 // Rota Principal
 app.get('/', (req, res) => {
-    res.render('formulario');
+    // SQL
+    let sql = "select * from produtos";
+
+    // executar o SQL
+    conexao.query(sql, function(erro, retorno){
+        res.render('formulario', {produtos:retorno});
+    })
 });
 
 // Rota de cadastro
 app.post('/cadastrar', function(req,res){
-    console.log(req.body);
-    res.end();
+    // Obter os dados que serão utilizados para o cadastro
+    let nome = req.body.nome;
+    let valor = req.body.valor;
+    let imagem = req.files.imagem.name;
+
+    // SQL
+    let sql = `INSERT INTO produtos (nome, valor, imagem) VALUES ('${nome}', ${valor}, '${imagem}')`;
+
+    // Executar o comando SQL
+    conexao.query(sql, function(erro, retorno){
+        // caso ocorra algum erro
+        if(erro) throw erro;
+
+        // Caso o cadastro seja feito com sucesso
+        req.files.imagem.mv(__dirname + '/imagens/' + req.files.imagem.name);
+        console.log(retorno);
+        res.end();
+    });
+
+    //Retornar para a rota principal
+    res.redirect('/');
+    
 })
 
 // Servidor
