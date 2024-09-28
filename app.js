5ea3158482596e8b0440e36aaf16614fc83568f6
André Silveira <andresilveira.email@gmail.com>
2024-09-28 10:48:14 -0300

    Formulário e Select
    Alteração para o formulário em dois arquivos diferentes, separando o forms e as imagens dos produtos e inserindo um campo de "select" para a categoria

--- app.js
+++ app.js
@@ -86,15 +86,16 @@ app.post('/cadastrar', function(req,res){
         // Obter os dados que serão utilizados para o cadastro
         let nome = req.body.nome;
         let valor = req.body.valor;
+        let categoria = req.body.categoria;
         let imagem = req.files.imagem.name;
 
         //Validar o nome do produto e o valor
-        if(nome == '' || valor == '' || isNaN(valor)){
+        if(nome == '' || valor == '' || isNaN(valor) || categoria == ''){
             res.redirect('/falhaCadastro');
         }
         else{
             // SQL
-            let sql = `INSERT INTO produtos (nome, valor, imagem) VALUES ('${nome}', ${valor}, '${imagem}')`;
+            let sql = `INSERT INTO produtos (nome, valor, imagem, categoria) VALUES ('${nome}', ${valor}, '${imagem}', '${categoria}')`;
             // Executar o comando SQL
             conexao.query(sql, function(erro, retorno){
                 // caso ocorra algum erro
